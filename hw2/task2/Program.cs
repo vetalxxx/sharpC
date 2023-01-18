7959e6e74e769bda2f8d4a54167545d0b926c2c5
Vitaly <vetalxxx2011@mail.ru>
2023-01-18 16:43:43 +0400

    the third of many

---
+++ hw2/task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int num = -1;
+while (number > 999)
+{
+  number = number / 10;
+  num = number % 10;
+}
+Console.WriteLine($"Получаем такое число: {number}");
+if (number / 100 < 1)
+{
+  Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+  num = number % 10;
+  Console.WriteLine($"Наша искомая третья цифра из введенного числа: {num}");
+}
+
+
