03be47b6d7e7f0f8219a2be9086876cb9f3b766b
Vitaly <vetalxxx2011@mail.ru>
2023-01-22 16:59:34 +0400

    TableCubes

---
+++ hw3/task23/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int GetNum(string text)
+{
+  Console.Write(text);
+  return int.Parse(Console.ReadLine());
+}
+
+void TableCubes(int num)
+{
+  int i = 1;
+  while (i < num)
+  {
+    Console.Write(Math.Pow(i, 3) + ", ");
+    i++;
+  }
+  Console.Write(Math.Pow(i, 3));
+}
+
+int number = GetNum("Введите число: ");
+
+TableCubes(number);
