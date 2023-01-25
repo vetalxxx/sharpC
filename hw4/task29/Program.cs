914c6fbe07262ebc7accaa9b82d1363247d3dad7
Vitaly <vetalxxx2011@mail.ru>
2023-01-25 14:15:48 +0400

    Get and Show array

---
+++ hw4/task29/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void ShowArray(int[] arr)
+{
+  int length = arr.Length - 1;
+  Console.Write("[");
+  for (int i = 0; i < length; i++)
+  {
+    Console.Write(arr[i] + ", ");
+  }
+  Console.Write(arr[length] + "]");
+}
+
+int[] GetArray(int number)
+{
+  int[] array = new int[number];
+  int length = array.Length;
+  for (int i = 0; i < length; i++)
+  {
+    array[i] = GetNumArr($"Введи {i + 1}-ое число в массив: ");
+  }
+  return array;
+}
+
+int GetNumArr(string text)
+{
+  Console.Write(text);
+  return int.Parse(Console.ReadLine());
+}
+
+int num = GetNumArr("Из скольки элементов состоит массив?: ");
+ShowArray(GetArray(num));
