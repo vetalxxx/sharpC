46eb2ba738c8379cbbfda21c61f7125a25014592
Vitaly <vetalxxx2011@mail.ru>
2023-01-25 12:41:25 +0400

    GetDegree

---
+++ hw4/task25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+void GetDegree(int numberA, int numberB)
+{
+  int result = 1;
+  for (int i = 1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+  Console.WriteLine(numberA + ", " + numberB + " -> " + result);
+}
+
+int GetNum(string text)
+{
+  Console.Write(text);
+  return int.Parse(Console.ReadLine());
+}
+
+int numA = GetNum("Введите число А: ");
+int numB = GetNum("В какую степень возвести число А?: ");
+
+GetDegree(numA, numB);
