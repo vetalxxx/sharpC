9ee62b4c679a66fa064e65b9cb48787c7d9a9ee7
Vitaly <vetalxxx2011@mail.ru>
2023-01-29 14:55:14 +0400

    quantity EvenNumbers

---
+++ hw5/task34/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int EvenNumbers(int[] arr)
+{
+  int count = 0;
+  int length = arr.Length;
+  for (int i = 0; i < length; i++)
+  {
+    if (arr[i] % 2 == 0) count += 1;
+  }
+  return count;
+}
+
+int[] FillArray(int number, int start, int finish)
+{
+  int[] arr = new int[number];
+  for (int i = 0; i < number; i++)
+  {
+    arr[i] = new Random().Next(start, finish);
+  }
+    return arr;
+}
+
+int GetNum(string text)
+{
+  Console.Write(text);
+  return int.Parse(Console.ReadLine());
+}
+
+int num = GetNum("Из скольки элементов состоит массив?: ");
+int[] array = FillArray(num, 100, 999);
+int quantity = EvenNumbers(array);
+Console.Write("[" + String.Join(", ", array) + "]");
+Console.Write(" -> " + quantity);
