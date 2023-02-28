ae2465dc96379cbf9cf8263c7fd2353e1242c635
Vitaly <vetalxxx2011@mail.ru>
2023-02-28 10:20:55 +0400

    sampling from an array

--- verification work/Program.cs
+++ verification work/Program.cs
@@ -9,4 +9,40 @@
 // ["1234", "1567", "-2", "computer science"] -> [ "-2" ]
 // ["Russia", "Denmark", "Kazan"] -> [ ]
 
+string[] NewArray(string[] arr)
+{
+  string[] array = new string[arr.Length];
+  int count = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i].Length <= 3)
+    {
+      array[count] = arr[i];
+      count++;
+    }
+  }
+  Array.Resize(ref array, count);
+  return array;
+}
 
+string[] GetArray(int number)
+{
+  string[] arr = new string[number];
+  for (int i = 0; i < number; i++)
+  {
+    arr[i] = GetString($"Введите {i + 1} - строку в ваш массив: ");
+  }
+  return arr;
+}
+
+string GetString(string text)
+{
+  Console.Write(text);
+  return Convert.ToString(Console.ReadLine());
+}
+
+int numberRows = int.Parse(GetString("Введите количество строк, которые вам необходимы: "));
+string[] array = GetArray(numberRows);
+Console.Write("[" + String.Join(", ", array) + "] -> ");
+string[] arrayNew = NewArray(array);
+Console.WriteLine("[" + String.Join(", ", arrayNew) + "]");
